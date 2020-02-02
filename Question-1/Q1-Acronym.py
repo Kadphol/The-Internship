2838e552e889c2cd11f29825c76035cb2df9c6e7
Kadphol <kadphol.len@mail.kmutt.ac.th>
2020-02-02 21:13:40 +0700

    Add sorting and print output

--- Question-1/Q1-Acronym.py
+++ Question-1/Q1-Acronym.py
@@ -1,11 +1,18 @@
+"""
+Sorting Acronyms
+Input:  the first line contain integer N, the number of name input.
+        the next N line contain string of name.
+Output: Print the acronyms in each line with sorting by longest to shortest.
+        if there are the same length, sorted by alphabetical order.
+"""
+
 def get_upper(name):
     word = []
     upper = ""
     word = name.split()
     for s in word:
-        if(s[0].isupper()):
+        if(s[0].isupper()): #check if split word is upper
             upper+=s[0]
-    print(upper)
     return upper
 
 def get_input(number):
@@ -15,14 +22,14 @@ def get_input(number):
         lines.append(line)
     return(lines)
 
-
 def main():
     number = int(input())
     name = get_input(number)
     acronym = []
     for n in name:
-        acronym.append(get_upper(n))
-    
+        acronym.append(get_upper(n)) #append acronym from each word 
+    acronym.sort(key = lambda item: (-len(item),item)) #sort by alphabetical order and sort again by length with reverse order
+    print("\n".join(acronym))
 
 if (__name__ == "__main__"):
     main()
