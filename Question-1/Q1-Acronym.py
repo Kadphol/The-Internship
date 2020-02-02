f0ce46a8ff31efc4f3b0a52e5c73e6518158c43c
Kadphol <kadphol.len@mail.kmutt.ac.th>
2020-02-02 20:22:21 +0700

    Add get_upper function to get acronyms from string

--- Question-1/Q1-Acronym.py
+++ Question-1/Q1-Acronym.py
@@ -1,3 +1,13 @@
+def get_upper(name):
+    word = []
+    upper = ""
+    word = name.split()
+    for s in word:
+        if(s[0].isupper()):
+            upper+=s[0]
+    print(upper)
+    return upper
+
 def get_input(number):
     lines = []
     for i in range(number):
@@ -9,7 +19,10 @@ def get_input(number):
 def main():
     number = int(input())
     name = get_input(number)
-    print(name)
+    acronym = []
+    for n in name:
+        acronym.append(get_upper(n))
+    
 
 if (__name__ == "__main__"):
     main()
