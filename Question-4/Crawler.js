15996d0accd639ff6c66081919bb507c787c10c3
Kadphol <kadphol.len@mail.kmutt.ac.th>
2020-02-04 22:15:55 +0700

    Make web crawler module

---
+++ Question-4/Crawler.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+
+const url = 'https://theinternship.io';
+
+var crawler = function getCompanies(){
+    const link = [];
+    return axios(url).then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        $('div.partner').each(function () {
+            const logo = $(this).find('a > img').attr('src');
+            const text = $(this).find('span').text();
+            link.push({
+                logo,
+                text,
+            });
+        });
+        return link;
+    }).catch(console.error);
+}
+module.exports = crawler;
